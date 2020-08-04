e884b8cfb3a8d67117cb71948879676157fda0ca
Madhukar, Dara Ramaiah <mran1@allstate.com>
2020-08-05 02:05:01 +0530

    with nav bar completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h2>checkout</h2>
+          </Route>
+          <Route path='/login'>
+            <h2>Login</h2>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <h2>Home</h2>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
